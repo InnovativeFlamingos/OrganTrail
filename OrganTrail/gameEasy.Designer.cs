dbfed112862f6853de6b582ef513372d3ed2a862
Andrew <heldiggris7@gmail.com>
2018-02-21 13:28:33 -0600

    update EasyGame

--- OrganTrail/gameEasy.Designer.cs
+++ OrganTrail/gameEasy.Designer.cs
@@ -28,42 +28,118 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.gbEverything = new System.Windows.Forms.GroupBox();
+            this.lblTraveled = new System.Windows.Forms.Label();
+            this.lblLandmark = new System.Windows.Forms.Label();
+            this.lblFood = new System.Windows.Forms.Label();
+            this.lblHealth = new System.Windows.Forms.Label();
+            this.lblTime = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
             this.label5 = new System.Windows.Forms.Label();
             this.label4 = new System.Windows.Forms.Label();
             this.label1 = new System.Windows.Forms.Label();
-            this.label2 = new System.Windows.Forms.Label();
-            this.label3 = new System.Windows.Forms.Label();
-            this.label6 = new System.Windows.Forms.Label();
-            this.label7 = new System.Windows.Forms.Label();
-            this.label8 = new System.Windows.Forms.Label();
-            this.label9 = new System.Windows.Forms.Label();
-            this.label10 = new System.Windows.Forms.Label();
-            this.label11 = new System.Windows.Forms.Label();
-            this.label12 = new System.Windows.Forms.Label();
-            this.label13 = new System.Windows.Forms.Label();
-            this.groupBox1.SuspendLayout();
+            this.lblSpace = new System.Windows.Forms.Label();
+            this.lblEnter = new System.Windows.Forms.Label();
+            this.picCharacter = new System.Windows.Forms.PictureBox();
+            this.gbEverything.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.picCharacter)).BeginInit();
             this.SuspendLayout();
             // 
-            // groupBox1
+            // gbEverything
             // 
-            this.groupBox1.BackColor = System.Drawing.SystemColors.ButtonFace;
-            this.groupBox1.Controls.Add(this.label13);
-            this.groupBox1.Controls.Add(this.label12);
-            this.groupBox1.Controls.Add(this.label11);
-            this.groupBox1.Controls.Add(this.label10);
-            this.groupBox1.Controls.Add(this.label9);
-            this.groupBox1.Controls.Add(this.label8);
-            this.groupBox1.Controls.Add(this.label7);
-            this.groupBox1.Controls.Add(this.label6);
-            this.groupBox1.Controls.Add(this.label5);
-            this.groupBox1.Controls.Add(this.label4);
-            this.groupBox1.Controls.Add(this.label1);
-            this.groupBox1.Location = new System.Drawing.Point(3, 210);
-            this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Size = new System.Drawing.Size(703, 139);
-            this.groupBox1.TabIndex = 0;
-            this.groupBox1.TabStop = false;
+            this.gbEverything.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.gbEverything.Controls.Add(this.lblTraveled);
+            this.gbEverything.Controls.Add(this.lblLandmark);
+            this.gbEverything.Controls.Add(this.lblFood);
+            this.gbEverything.Controls.Add(this.lblHealth);
+            this.gbEverything.Controls.Add(this.lblTime);
+            this.gbEverything.Controls.Add(this.label8);
+            this.gbEverything.Controls.Add(this.label7);
+            this.gbEverything.Controls.Add(this.label6);
+            this.gbEverything.Controls.Add(this.label5);
+            this.gbEverything.Controls.Add(this.label4);
+            this.gbEverything.Controls.Add(this.label1);
+            this.gbEverything.Location = new System.Drawing.Point(3, 210);
+            this.gbEverything.Name = "gbEverything";
+            this.gbEverything.Size = new System.Drawing.Size(703, 139);
+            this.gbEverything.TabIndex = 0;
+            this.gbEverything.TabStop = false;
+            // 
+            // lblTraveled
+            // 
+            this.lblTraveled.Font = new System.Drawing.Font("OCR A Extended", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTraveled.Location = new System.Drawing.Point(323, 106);
+            this.lblTraveled.Name = "lblTraveled";
+            this.lblTraveled.Size = new System.Drawing.Size(108, 23);
+            this.lblTraveled.TabIndex = 11;
+            this.lblTraveled.Text = "0 miles";
+            // 
+            // lblLandmark
+            // 
+            this.lblLandmark.Font = new System.Drawing.Font("OCR A Extended", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblLandmark.Location = new System.Drawing.Point(323, 83);
+            this.lblLandmark.Name = "lblLandmark";
+            this.lblLandmark.Size = new System.Drawing.Size(108, 23);
+            this.lblLandmark.TabIndex = 10;
+            this.lblLandmark.Text = "0 miles";
+            // 
+            // lblFood
+            // 
+            this.lblFood.Font = new System.Drawing.Font("OCR A Extended", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblFood.Location = new System.Drawing.Point(323, 60);
+            this.lblFood.Name = "lblFood";
+            this.lblFood.Size = new System.Drawing.Size(122, 23);
+            this.lblFood.TabIndex = 9;
+            this.lblFood.Text = "0 pounds";
+            // 
+            // lblHealth
+            // 
+            this.lblHealth.Font = new System.Drawing.Font("OCR A Extended", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblHealth.Location = new System.Drawing.Point(323, 35);
+            this.lblHealth.Name = "lblHealth";
+            this.lblHealth.Size = new System.Drawing.Size(138, 23);
+            this.lblHealth.TabIndex = 8;
+            // 
+            // lblTime
+            // 
+            this.lblTime.Font = new System.Drawing.Font("OCR A Extended", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTime.Location = new System.Drawing.Point(323, 12);
+            this.lblTime.Name = "lblTime";
+            this.lblTime.Size = new System.Drawing.Size(80, 23);
+            this.lblTime.TabIndex = 7;
+            this.lblTime.Text = "00:00";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("OCR A Extended", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label8.Location = new System.Drawing.Point(108, 104);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(220, 23);
+            this.label8.TabIndex = 6;
+            this.label8.Text = "Miles traveled:";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("OCR A Extended", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Location = new System.Drawing.Point(122, 81);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(206, 23);
+            this.label7.TabIndex = 5;
+            this.label7.Text = "Next landmark:";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("OCR A Extended", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(248, 58);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(80, 23);
+            this.label6.TabIndex = 4;
+            this.label6.Text = "Food:";
             // 
             // label5
             // 
@@ -97,109 +173,40 @@
             this.label1.Text = "Press SPACE BAR to continue";
             this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             // 
-            // label2
+            // lblSpace
             // 
-            this.label2.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
-            this.label2.Font = new System.Drawing.Font("OCR A Extended", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.ForeColor = System.Drawing.SystemColors.ButtonFace;
-            this.label2.Location = new System.Drawing.Point(3, 345);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(703, 31);
-            this.label2.TabIndex = 2;
-            this.label2.Text = "Press SPACE BAR to continue";
-            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
-            this.label2.Visible = false;
+            this.lblSpace.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.lblSpace.Font = new System.Drawing.Font("OCR A Extended", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblSpace.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.lblSpace.Location = new System.Drawing.Point(3, 345);
+            this.lblSpace.Name = "lblSpace";
+            this.lblSpace.Size = new System.Drawing.Size(703, 31);
+            this.lblSpace.TabIndex = 2;
+            this.lblSpace.Text = "Press SPACE BAR to continue";
+            this.lblSpace.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.lblSpace.Visible = false;
             // 
-            // label3
+            // lblEnter
             // 
-            this.label3.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
-            this.label3.Font = new System.Drawing.Font("OCR A Extended", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label3.ForeColor = System.Drawing.SystemColors.ButtonFace;
-            this.label3.Location = new System.Drawing.Point(3, 194);
-            this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(703, 23);
-            this.label3.TabIndex = 3;
-            this.label3.Text = "Press ENTER to size up the situation";
-            this.label3.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.lblEnter.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.lblEnter.Font = new System.Drawing.Font("OCR A Extended", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblEnter.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.lblEnter.Location = new System.Drawing.Point(3, 194);
+            this.lblEnter.Name = "lblEnter";
+            this.lblEnter.Size = new System.Drawing.Size(703, 23);
+            this.lblEnter.TabIndex = 3;
+            this.lblEnter.Text = "Press ENTER to size up the situation";
+            this.lblEnter.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             // 
-            // label6
+            // picCharacter
             // 
-            this.label6.AutoSize = true;
-            this.label6.Font = new System.Drawing.Font("OCR A Extended", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label6.Location = new System.Drawing.Point(248, 58);
-            this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(80, 23);
-            this.label6.TabIndex = 4;
-            this.label6.Text = "Food:";
-            // 
-            // label7
-            // 
-            this.label7.AutoSize = true;
-            this.label7.Font = new System.Drawing.Font("OCR A Extended", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label7.Location = new System.Drawing.Point(122, 81);
-            this.label7.Name = "label7";
-            this.label7.Size = new System.Drawing.Size(206, 23);
-            this.label7.TabIndex = 5;
-            this.label7.Text = "Next landmark:";
-            // 
-            // label8
-            // 
-            this.label8.AutoSize = true;
-            this.label8.Font = new System.Drawing.Font("OCR A Extended", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label8.Location = new System.Drawing.Point(108, 104);
-            this.label8.Name = "label8";
-            this.label8.Size = new System.Drawing.Size(220, 23);
-            this.label8.TabIndex = 6;
-            this.label8.Text = "Miles traveled:";
-            // 
-            // label9
-            // 
-            this.label9.AutoSize = true;
-            this.label9.Font = new System.Drawing.Font("OCR A Extended", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label9.Location = new System.Drawing.Point(323, 12);
-            this.label9.Name = "label9";
-            this.label9.Size = new System.Drawing.Size(80, 23);
-            this.label9.TabIndex = 7;
-            this.label9.Text = "00:00";
-            // 
-            // label10
-            // 
-            this.label10.Font = new System.Drawing.Font("OCR A Extended", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label10.Location = new System.Drawing.Point(323, 35);
-            this.label10.Name = "label10";
-            this.label10.Size = new System.Drawing.Size(138, 23);
-            this.label10.TabIndex = 8;
-            this.label10.Text = "very poor";
-            // 
-            // label11
-            // 
-            this.label11.AutoSize = true;
-            this.label11.Font = new System.Drawing.Font("OCR A Extended", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label11.Location = new System.Drawing.Point(323, 60);
-            this.label11.Name = "label11";
-            this.label11.Size = new System.Drawing.Size(122, 23);
-            this.label11.TabIndex = 9;
-            this.label11.Text = "0 pounds";
-            // 
-            // label12
-            // 
-            this.label12.AutoSize = true;
-            this.label12.Font = new System.Drawing.Font("OCR A Extended", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label12.Location = new System.Drawing.Point(323, 83);
-            this.label12.Name = "label12";
-            this.label12.Size = new System.Drawing.Size(108, 23);
-            this.label12.TabIndex = 10;
-            this.label12.Text = "0 miles";
-            // 
-            // label13
-            // 
-            this.label13.AutoSize = true;
-            this.label13.Font = new System.Drawing.Font("OCR A Extended", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label13.Location = new System.Drawing.Point(323, 106);
-            this.label13.Name = "label13";
-            this.label13.Size = new System.Drawing.Size(108, 23);
-            this.label13.TabIndex = 11;
-            this.label13.Text = "0 miles";
+            this.picCharacter.Image = global::OrganTrail.Properties.Resources.Sub_clone1;
+            this.picCharacter.Location = new System.Drawing.Point(527, 44);
+            this.picCharacter.Name = "picCharacter";
+            this.picCharacter.Size = new System.Drawing.Size(149, 133);
+            this.picCharacter.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.picCharacter.TabIndex = 4;
+            this.picCharacter.TabStop = false;
             // 
             // gameEasy
             // 
@@ -207,32 +214,35 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
             this.ClientSize = new System.Drawing.Size(709, 377);
-            this.Controls.Add(this.label3);
-            this.Controls.Add(this.label2);
-            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.picCharacter);
+            this.Controls.Add(this.lblEnter);
+            this.Controls.Add(this.lblSpace);
+            this.Controls.Add(this.gbEverything);
             this.Name = "gameEasy";
             this.Text = "gameEasy";
-            this.groupBox1.ResumeLayout(false);
-            this.groupBox1.PerformLayout();
+            this.gbEverything.ResumeLayout(false);
+            this.gbEverything.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.picCharacter)).EndInit();
             this.ResumeLayout(false);
 
         }
 
         #endregion
 
-        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.GroupBox gbEverything;
         private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label lblSpace;
+        private System.Windows.Forms.Label lblEnter;
         private System.Windows.Forms.Label label5;
         private System.Windows.Forms.Label label4;
-        private System.Windows.Forms.Label label13;
-        private System.Windows.Forms.Label label12;
-        private System.Windows.Forms.Label label11;
-        private System.Windows.Forms.Label label10;
-        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label lblTraveled;
+        private System.Windows.Forms.Label lblLandmark;
+        private System.Windows.Forms.Label lblFood;
+        private System.Windows.Forms.Label lblHealth;
+        private System.Windows.Forms.Label lblTime;
         private System.Windows.Forms.Label label8;
         private System.Windows.Forms.Label label7;
         private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.PictureBox picCharacter;
     }
 }
