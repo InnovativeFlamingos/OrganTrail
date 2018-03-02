6258e77669b2d2079bdbc957c1417f32dbf2db9a
Andrew <heldiggris7@gmail.com>
2018-03-02 14:53:22 -0600

    update EasyGame

--- OrganTrail/Properties/Resources.Designer.cs
+++ OrganTrail/Properties/Resources.Designer.cs
@@ -83,6 +83,16 @@ namespace OrganTrail.Properties {
         /// <summary>
         ///   Looks up a localized resource of type System.Drawing.Bitmap.
         /// </summary>
+        internal static System.Drawing.Bitmap Sub2 {
+            get {
+                object obj = ResourceManager.GetObject("Sub2", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
         internal static System.Drawing.Bitmap Untitled_document__1_ {
             get {
                 object obj = ResourceManager.GetObject("Untitled document (1)", resourceCulture);
